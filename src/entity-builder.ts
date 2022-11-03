edb9eed918ee3469c60196ddb3d9a08b308edb75
Marco Alka <me@marco-alka.de>
2022-11-03 13:01:13 +0100

    align entity-builder behavior with other builders

--- src/entity-builder.ts
+++ src/entity-builder.ts
@@ -5,28 +5,32 @@ import {TObjectProto} from "./_.spec";
 export * from './entity-builder.spec';
 
 export class EntityBuilder implements IEntityBuilder {
-    protected entity: Entity;
+    protected components = new Map<Object | TObjectProto, unknown[]>();
 
     constructor(
-        uuid?: string,
+        protected uuid?: string,
         protected callback?: (entity: Entity) => void
-    ) {
-        this.entity = new Entity(uuid);
-    }
+    ) {}
 
     build(): Entity {
-        this.callback?.(this.entity);
-        return this.entity;
+        const entity = new Entity(this.uuid);
+        let component;
+
+        for (component of this.components) {
+            entity.addComponent(component[0], ...component[1]);
+        }
+
+        this.callback?.(entity);
+        return entity;
     }
 
     with(component: Object | TObjectProto, ...args: unknown[]): EntityBuilder {
-        this.entity.addComponent(component, ...args);
+        this.components.set(component, args);
         return this;
     }
 
     withAll(...components: (Object | TObjectProto)[]): EntityBuilder {
         let component;
-
         for (component of components) {
             this.with(component);
         }
