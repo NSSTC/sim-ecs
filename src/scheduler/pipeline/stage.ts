79accca4dfef3a11cf8b63d891306edc6c1d68ea
Marco Alka <me@marco-alka.de>
2022-12-29 13:28:12 +0100

    invert stage executor try-loop
    micro optimization

--- src/scheduler/pipeline/stage.ts
+++ src/scheduler/pipeline/stage.ts
@@ -10,16 +10,16 @@ export * from "./stage.spec";
 export async function defaultStageSchedulingAlgorithm(systems: ReadonlyArray<Readonly<ISystem>>, eventBus: Readonly<IEventBus>): Promise<void> {
     let system;
 
-    for (system of systems) {
-        try {
+    try {
+        for (system of systems) {
             // todo: check WRITE constraints to speed it up...
             await system.runFunction.call(system, system[systemRunParamSym]!);
-        } catch (error) {
-            if (error instanceof Error && !!system) {
-                await eventBus.publish(new SystemError(error, system.constructor as TTypeProto<ISystem>));
-            } else {
-                throw error;
-            }
+        }
+    } catch (error) {
+        if (error instanceof Error && !!system) {
+            await eventBus.publish(new SystemError(error, system.constructor as TTypeProto<ISystem>));
+        } else {
+            throw error;
         }
     }
 }
