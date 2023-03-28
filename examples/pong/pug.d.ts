bb0bc4336e98490dd13418456fe7092914bf6ce2
Marco Alka <me@marco-alka.de>
2023-03-28 22:46:18 +0200

    clean up and update deps

--- examples/pong/pug.d.ts
+++
@@ -1,5 +0,0 @@
-declare module '*.pug' {
-    type TPugTemplateData = { [key: string]: any };
-    const template: (data?: TPugTemplateData) => string;
-    export = template;
-}
--- examples/pong/scss.d.ts
+++
@@ -1,4 +0,0 @@
-declare module '*.scss' {
-    const css: string;
-    export = css;
-}
