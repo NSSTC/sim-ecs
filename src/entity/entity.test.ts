00fd039b2373b33ab74220256d7169d7cc7f816f
Marco Alka <me@marco-alka.de>
2023-07-07 23:04:13 +0200

    Add unit tests for entity events

--- src/entity/entity.test.ts
+++ src/entity/entity.test.ts
@@ -47,4 +47,82 @@ describe('Test Entity', () => {
         entity.addComponent(component);
         expect(entity.addComponent.bind(entity, component)).to.throw();
     });
+
+    it('Events: AddComponent', () => {
+        const component = {};
+        const entity = new Entity();
+        let eventCalled = false;
+
+        entity.addEventListener("addComponent", event => {
+            assert.equal(event.componentInstance, component);
+            eventCalled = true;
+        });
+
+        entity.addComponent(component);
+        assert.equal(eventCalled, true);
+    });
+
+    it('Events: AddTag', () => {
+        const tag = 'awww';
+        const entity = new Entity();
+        let eventCalled = false;
+
+        entity.addEventListener("addTag", event => {
+            assert.equal(event.tag, tag);
+            eventCalled = true;
+        });
+
+        entity.addTag(tag);
+        assert.equal(eventCalled, true);
+    });
+
+    it('Events: Clone', () => {
+        const component = {
+            foo: 42,
+        };
+        const entity = new Entity();
+        let eventCalled = false;
+
+        entity.addEventListener("clone", event => {
+            assert.equal(event.original, entity);
+            assert.equal(event.clone.getComponents().next()!.value.foo, component.foo);
+            eventCalled = true;
+        });
+
+        entity.addComponent(component);
+        entity.clone(new SerDe());
+        assert.equal(eventCalled, true);
+    });
+
+    it('Events: RemoveComponent', () => {
+        const component = {};
+        const entity = new Entity();
+        let eventCalled = false;
+
+        entity.addEventListener("removeComponent", event => {
+            assert.equal(event.componentInstance, component);
+            eventCalled = true;
+        });
+
+        entity.addComponent(component);
+        assert.equal(eventCalled, false);
+        entity.removeComponent(component);
+        assert.equal(eventCalled, true);
+    });
+
+    it('Events: RemoveTag', () => {
+        const tag = 'awww';
+        const entity = new Entity();
+        let eventCalled = false;
+
+        entity.addEventListener("removeTag", event => {
+            assert.equal(event.tag, tag);
+            eventCalled = true;
+        });
+
+        entity.addTag(tag);
+        assert.equal(eventCalled, false);
+        entity.removeTag(tag);
+        assert.equal(eventCalled, true);
+    });
 });
